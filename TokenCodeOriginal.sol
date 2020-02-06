194fb4fc23cb5554fa06464b5da6cdec68b9a7eb
Denis Putnam <denisputnam@verizon.net>
2020-02-05 22:36:33 -0500

    Change the inital value to be large.

--- TokenCodeOriginal.sol
+++ TokenCodeOriginal.sol
@@ -107,11 +107,11 @@ contract DenisTestToken is StandardToken { // CHANGE THIS. Update the contract n
     // This is a constructor function
     // which means the following function name has to match the contract name declared above
     function DenisTestToken() {
-        //balances[msg.sender] = 1000000000000000000000;             // Give the creator all initial tokens. This is set to 1000 for example.
-        balances[msg.sender] = 1000;                                 // Give the creator all initial tokens. This is set to 1000 for example.
+        balances[msg.sender] = 1000000000000000000000;             // Give the creator all initial tokens. This is set to 1000 for example.
+        //balances[msg.sender] = 1000;                                 // Give the creator all initial tokens. This is set to 1000 for example.
                                                                      // If you want your initial tokens to be X and your decimal is 5, set this value to X * 100000. (CHANGE THIS)
-        //totalSupply = 1000000000000000000000;                      // Update total supply (1000 for example) (CHANGE THIS)
-        totalSupply = 1000;                                          // Update total supply (1000 for example) (CHANGE THIS)
+        totalSupply = 1000000000000000000000;                      // Update total supply (1000 for example) (CHANGE THIS)
+        //totalSupply = 1000;                                          // Update total supply (1000 for example) (CHANGE THIS)
         name = "DenisTestToken";                                     // Set the name for display purposes (CHANGE THIS)
         decimals = 18;                                               // Amount of decimals for display purposes (CHANGE THIS)
         symbol = "DTTS";                                             // Set the symbol for display purposes (CHANGE THIS)
